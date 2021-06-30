6c262c4e4bc7d51b8d9fc3a8ebdf13ca00af1d8d
jxm90 <joemax1990@gmail.com>
2021-06-30 16:51:50 -0500

    Update phpadmin-user.sql

--- phpadmin-user.sql
+++ phpadmin-user.sql
@@ -1,2 +1,2 @@
-CREATE USER '$username'@'%' IDENTIFIED BY '$password';
-GRANT ALL PRIVILEGES ON *.* TO '$username'@'%' WITH GRANT OPTION;
+CREATE USER 'username'@'%' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON *.* TO 'username'@'%' WITH GRANT OPTION;
