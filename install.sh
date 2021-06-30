64ec843b6795743fc41e0a972de4076d41d943ae
jxm90 <joemax1990@gmail.com>
2021-06-29 22:26:14 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -28,3 +28,16 @@ sudo service apache2 restart
 #Web Permissions
 sudo chown -R pi:www-data /var/www/html/
 sudo chmod -R 770 /var/www/html/
+
+#Create phpmyadmin user with all options
+#read -p "Username: " username
+#while true; do
+#    read -p "Password: " password
+#    echo
+#    read -p "Password (again): " password2
+#    echo
+#    [ "$password" = "$password2" ] && break
+#    echo "Please try again"
+#done
+#sudo mysql -u root -p -e "CREATE USER '$username'@'%' IDENTIFIED BY '$password';"
+#sudo mysql -u root -p -e "GRANT ALL PRIVILEGES ON *.* TO '$username'@'%' WITH GRANT OPTION;"
